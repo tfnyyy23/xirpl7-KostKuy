d4538826ee1813c59aee1e944ea39dd743ee1a21
Shinta Nuriyah <snuriyah@MacBook-Air-Shinta.local>
2021-11-08 11:01:59 +0700

    first commit

---
+++ masuk.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/masuk.css">
+    <link rel="icon" href="assets/img/logo.png">
+    <title>Masuk || Kost Kuy</title>
+</head>
+<body>
+<div class = "bg">
+        <nav>
+            <img class="vektor" src="assets/img/vektor1.svg"/>
+            <img class="alir" src="assets/img/alir1.svg" />
+            <img class ="bginti" src="assets/img/bg1.svg" />
+        </nav>
+    </div>
+</body>
+</html>
